dba4f887a5fc547f3b9de2990b2b65c3015ad4d7
Vicky Li <VickyL4198@gmail.com>
2024-02-19 16:24:29 -0800

    completed assignment

---
+++ js/script.js
@@ -0,0 +1,162 @@
+// STEP 1
+  /*
+  let someMonth
+  function theMonth ()
+  let currentMonth
+  let summerMonth
+  let myLibraryFunction  
+  */
+
+// STEP 2
+  /*
+  1.5 
+  'Great'
+  true
+  null
+  */
+
+// STEP 3
+  /*
+  let sumOfNums = 1 + 2
+  let differences = 5 - 3
+  */
+
+// STEP 4
+  /*
+  let firstName;
+  let lastName;
+  let address;
+  let city;
+  let state;
+  let zipCode;
+  let yourAge;
+  let referralSource;
+  let strMayWeContactYou; 
+  */
+
+// STEP 5
+  /*
+  let firstName = 'Vicky';
+  let state = 'CA';
+  let state = 'CA', zipCode = 92126;
+  */
+
+// STEP 6
+  /*
+  let sum = 2 + 'cool';
+  console.log(sum);
+  */
+
+// STEP 7
+  /*
+  let variable1 = true + 'hello';
+  console.log(variable1);
+
+  let variable2 = 3 + true;
+  console.log(variable2);
+  */
+
+// STEP 8
+  /*
+  let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+  console.log(someString);
+  */
+
+// STEP 9
+  /*
+  let nullValue = null;
+  console.log(nullValue);
+
+  let undefinedValue;
+  console.log(undefinedValue);
+  */
+
+// STEP 10
+  /*
+  console.log(typeof('hello'));
+  console.log(typeof(8));
+  console.log(typeof(true));
+  let score = {test1: 90, test2: 85};
+  console.log(typeof(score));
+  let undefinedValue;
+  console.log(typeof(undefinedValue));
+  */
+
+// STEP 11
+  /*
+  alert('Hello' + ' Vicky Li' + ', welcome to the Javascript class!');
+  */
+
+// STEP 12
+  /*
+  let name = 'Vicky Li';
+  alert(`Hello ${name}, welcome to the Javascript class!`);
+  */
+
+// STEP 13
+  /*
+  let name = 'Vicky Li';
+  let course = 'JavaScript';
+  alert(`Hello ${name}, welcome to the ${course} class!`);
+  */
+
+// STEP 14
+  /*
+  let name = 'Vicky Li';
+  let course = 'JavaScript';
+  alert(`Hello ${name}.\nWelcome to the ${course} class!`);
+  */
+
+// STEP 15
+  /*
+  let name = prompt('What is your name?');
+  let course = 'JavaScript';
+  alert(`Hello ${name}.\nWelcome to the ${course} class!`);
+  */
+
+// STEP 16
+  /*
+  let name = prompt('What is your name?');
+  let course = prompt('What course are you taking?');
+  alert(`Hello ${name}.\nWelcome to the ${course} class!`);
+  */
+
+// STEP 17
+  /*
+  let x = 10;
+  let y = 20;
+  console.log(x + y);
+  */
+
+// STEP 18
+  /*
+  let x = 20;
+  x += 20;
+  console.log(x);
+  */
+
+// STEP 19
+  /*
+  let x = 20;
+  x *= 5;
+  console.log(x);
+  */
+
+// STEP 20
+  /*
+  let x = 20 % 3;
+  x /= 1;
+  console.log(x);
+  */
+
+// STEP 21
+  /*
+  let x = 10 > 5;
+  console.log(x);
+  */
+
+// STEP 22
+  /*
+  let x = 20 <= 12;
+  console.log(x);
+  */
